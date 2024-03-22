76c5f0468740382fe46094e6e948fa599d421316
Raul Bordon <raul_bordon11@hotmail.com.ar>
2024-03-21 22:13:39 -0300

    ParteFront

--- back-Blog/controller/controllers.js
+++ back-Blog/controller/controllers.js
@@ -2,6 +2,7 @@ import sequelize from 'sequelize';
 import {tbComentario,tbPelicula,tbComentarioPelicula} from "../database/module.js";
 
 export const AddPelicula = async (req, res) => {
+    console.log("patata1")
     try {
         const { nombrePelicula, posterPelicula, descripcionPelicula } = req.body;
         const AddingCat = await tbPelicula.create({ nombrePelicula, posterPelicula, descripcionPelicula });
@@ -30,3 +31,33 @@ export const AddComentarioPelicula = async (req, res) => {
         res.json({ msg: err.message });
     }
 };
+
+export const listPeliculas = async (req, res) => {
+    try {
+        const listaPeli = await tbPelicula.findAll();
+        return res.json({jsonPeli:listaPeli});
+        res.json({ msg: "Creado correctamente" });
+    } catch (err) {
+        res.json({ msg: err.message });
+    }
+}
+
+export const listComentarios = async (req, res) => {
+    try {
+        const listComentario = await tbComentario.findAll();
+        return res.json({jsonComentario:listComentario});
+        res.json({ msg: "Creado correctamente" });
+    } catch (err) {
+        res.json({ msg: err.message });
+    }
+}
+
+export const listComentariosPelis = async (req, res) => {
+    try {
+        const listComentarioPeli = await tbComentarioPelicula.findAll();
+        return res.json({jsonComentarioPeli:listComentarioPeli});
+        res.json({ msg: "Creado correctamente" });
+    } catch (err) {
+        res.json({ msg: err.message });
+    }
+}
--- back-Blog/routes/routes.js
+++ back-Blog/routes/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { AddPelicula,AddComentario,AddComentarioPelicula } from '../controller/controllers.js';
+import { AddPelicula,AddComentario,AddComentarioPelicula,listPeliculas,listComentarios,listComentariosPelis } from '../controller/controllers.js';
 
 const rutas = express.Router();
 
 rutas.post('/peliculas/', AddPelicula);
 rutas.post('/comentarios/', AddComentario);
 rutas.post('/comentariosPeliculas/', AddComentarioPelicula);
+rutas.get('/peliculasL/', listPeliculas);
+rutas.get('/comentariosL/', listComentarios);
+rutas.get('/comentariosPeliculasL/', listComentariosPelis);
 
 export default rutas;
